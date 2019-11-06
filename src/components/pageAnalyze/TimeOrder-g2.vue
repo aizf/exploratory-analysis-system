a1e83aa1f26856f14837cf8714135ecfd2b19534
aizf <aizf123000@outlook.com>
2019-11-06 23:45:52 +0800

    1

---
+++ src/components/pageAnalyze/TimeOrder-g2.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="operations"></div>
+</template>
+
+<script>
+import * as d3 from "d3";
+import G2 from "@antv/g2";
+import { View } from "@antv/data-set";
+
+export default {
+  name: "TimeOrder",
+  data() {
+    return {
+      chart: {},
+      view: {},
+      view_: {}
+    };
+  },
+  computed: {
+    width() {
+      return this.$store.state.dpiX * 0.7;
+    },
+    height() {
+      return (this.$store.state.dpiY - 64) * 0.35;
+    },
+    visualData() {
+      return this.$store.state.visualData;
+    },
+    nodes() {
+      return this.$store.getters.nodes;
+    },
+    links() {
+      return this.$store.getters.links;
+    },
+    backgroundColor() {
+      return this.$store.state.backgroundColor;
+    },
+    colorPalette() {
+      return this.$store.state.colorPalette;
+    },
+    operations() {
+      return this.$store.state.operations;
+    },
+    operations_() {
+      return this.$store.state.operations_;
+    }
+  },
+  mounted() {
+    this.chart = new G2.Chart({
+      container: document.getElementsByClassName("operations")[0],
+      forceFit: true,
+      height: this.height,
+      theme: "dark"
+      // renderer : 'svg'
+    });
+
+    this.chart.legend({
+      title: null // 不展示图例的标题
+      // marker: "square" // 设置图例 marker 的显示样式
+    });
+    this.chart.legend("nodes", false); // 隐藏 nodes 维度对应的图例
+    this.chart.tooltip({
+      showTitle: false
+      // itemTpl: '<li>{color}{name}\t{value}</li>'
+    });
+    this.chart.scale("time", {
+      type: "time", // 指定 time 类型
+      mask: "HH:mm:ss", // 指定时间的输出格式
+      sync: true
+    });
+
+    const defs = {
+      time: {
+        type: "time", // 指定 time 类型
+        mask: "HH:mm:ss", // 指定时间的输出格式
+        sync: true
+      },
+      action: {
+        type: "cat", // 指定 cat 分类类型
+        values: this.$store.state.operationTypes // 重新指定 c 属性每一个的值
+      }
+    };
+    this.view = this.chart.view();
+    this.view.source(this.operations, defs);
+    this.view
+      .point()
+      .position("time*action")
+      .color("action")
+      .size("nodes", nodes => {
+        let size = Math.sqrt(nodes.length);
+        return size > 4.5 ? size : 4.5;
+      })
+      .opacity(0.8)
+      .shape("circle")
+      .tooltip("time*action", (time, action, nodes) => {
+        return {
+          name: action,
+          value: `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+        };
+        // , { "操作": action }];
+      });
+
+    const defs_ = {
+      time: {
+        type: "time", // 指定 time 类型
+        mask: "HH:mm:ss", // 指定时间的输出格式
+        sync: true
+      },
+      action: {
+        type: "cat", // 指定 cat 分类类型
+        values: this.$store.state.operation_Types // 重新指定 c 属性每一个的值
+      },
+      max: {
+        type: "cat", // 指定 cat 分类类型
+        values: this.$store.state.operationTypes
+      }
+    };
+    this.view_ = this.chart.view();
+    this.view_.source(this.operations_, defs_);
+    this.view_.axis("action", {
+      label: null
+    });
+    this.view_
+      .interval()
+      .position("time*max")
+      .shape("line")
+      .color("action", ["#F04864", "#B381E6", "#ea7e53"])
+      .size(2)
+      .opacity(0.3)
+      .tooltip("time*action", (time, action, nodes) => {
+        return {
+          name: action,
+          value: `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+        };
+        // , { "操作": action }];
+      });
+
+    this.chart.render();
+  },
+  activated() {
+    this.view.changeData(this.operations);
+    this.view_.changeData(this.operations_);
+  },
+  methods: {}
+};
+</script>
