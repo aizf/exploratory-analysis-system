d49ba205456f793b7042979c112b710a135181f8
aizf <45379838+aizf@users.noreply.github.com>
2019-12-25 02:56:05 +0800

    fixed dijkstra ::>_<::

--- src/store/getters.js
+++ src/store/getters.js
@@ -254,33 +254,39 @@ const getters = {
   dijkstra: () => (matrix, start = 0) => {
     const rows = matrix.length,
       cols = matrix[0].length;
+    // 剩余的indexes
+    const remaining = new Array(rows).fill(0).map((d, i) => i);
 
     if (rows !== cols || start >= rows) throw new Error("邻接矩阵错误或者源点错误");
 
     // 初始化distance,book
     // distance存储最短距离,book存储已经遍历的点
     const distance = new Array(rows);
-    for (let i = 0; i < rows; i++) {
-      distance[i] = { value: Infinity, path: [] };
+    for (let index = 0; index < rows; index++) {
+      distance[index] = { value: Infinity, path: [] };
       // value为路径长度，path路径
     }
-    // const book = [];
     distance[start].value = 0;
 
-    for (let i = 0; i < rows; i++) {
-      // book.push(i);
-      // 达到不了的顶点不能作为中转跳点
-      if (distance[i].value < Infinity) {
-        for (let j = 0; j < cols; j++) {
-          // 如果已经是最短路径的节点，则continue
-          // if (book.includes(j)) continue;
-          // 通过比较distance[i].value + matrix[i][j]和distance[j].value的大小来决定是否更新distance[j]
-          if (distance[i].value + matrix[i][j] < distance[j].value) {
-            distance[j].path = [...distance[i].path, i];
-            distance[j].value = matrix[i][j] + distance[i].value;
-          }
+    while (remaining.length > 1) {
+      const minVal = Math.min(...distance.filter((d, index) => remaining.includes(index)).map(d => d.value));
+      if (minVal >= Infinity) break;
+
+      const i = distance
+        .findIndex((d, index) => remaining.includes(index) && d.value === minVal); // 最小数的index
+
+      remaining.splice(remaining.indexOf(i), 1);
+
+      for (let j = 0; j < cols; j++) {
+        // 如果已经是最短路径的节点，则continue
+        if (!remaining.includes(j)) continue;
+        // 通过比较distance[i].value + matrix[i][j]和distance[j].value的大小来决定是否更新distance[j]
+        if (distance[i].value + matrix[i][j] < distance[j].value) {
+          distance[j].path = [...distance[i].path, i];
+          distance[j].value = matrix[i][j] + distance[i].value;
         }
       }
+
     }
     return distance;
   }
